460dad71c0c27554f98a9ccb151ab64b22917985
Gosfist <akunsekolah1234567@gmail.com>
2024-09-08 20:32:55 +0700

    halotes3

---
+++ soal3.java
@@ -0,0 +1,15 @@
+public class soal3 {
+    public static void main(String[] args) {
+
+    // input 
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+    // proses
+        int x = (2*a*a)+(a*b)+c;
+
+    // output
+        System.out.println("hasil: "+x);
+    }
+}
