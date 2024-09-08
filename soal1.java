b37dbc451c1144b363e78edb4b51e9883b320f6a
Gosfist <akunsekolah1234567@gmail.com>
2024-09-08 20:29:18 +0700

    soal1

---
+++ soal1.java
@@ -0,0 +1,14 @@
+public class soal1 {
+    public static void main(String[] args) {
+
+    // input 
+        int alas = 10;
+        int tinggi = 20;
+
+    // proses   
+        int luas = (alas*tinggi)/2;
+
+    // output
+        System.out.println("luas segitiga: "+luas);
+    }
+}
